b3538b9146c862df3593d8a59ac6c900373c7b3c
Rajesh Sola <rajeshsola@gmail.com>
2025-11-06 08:36:19 +0530

    Add tests for leap year checks

---
+++ src/test/java/org/osidays/demo/LeapTest.java
@@ -0,0 +1,30 @@
+package org.osidays.demo;
+
+import static org.osidays.demo.CalendarUtils.isLeapYear;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
+public class LeapTest
+{
+   @Test
+   public void normalLeapYears() {
+        assertTrue(CalendarUtils.isLeapYear(1996));
+        assertTrue(CalendarUtils.isLeapYear(2012));
+   }
+   @Test
+   public void normalNonLeapYears() {
+        assertFalse(CalendarUtils.isLeapYear(1995));
+        assertTrue(CalendarUtils.isLeapYear(2011));
+   }
+   @Test
+   public void centuryLeapYears() {
+        assertTrue(CalendarUtils.isLeapYear(2400));
+        assertTrue(CalendarUtils.isLeapYear(1600));
+   }
+   @Test
+   public void centuryNonLeapYears() {
+        assertFalse(CalendarUtils.isLeapYear(1700));
+        assertTrue(CalendarUtils.isLeapYear(2100));
+   }
+}
